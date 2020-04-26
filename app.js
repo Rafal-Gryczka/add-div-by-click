4daf2651006c60efddf001b95834bfbbb6477741
Rafal-Gryczka <63900821+Rafal-Gryczka@users.noreply.github.com>
2020-04-26 14:46:02 +0200

    create index.html and basic add

---
+++ app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("button");
+let number = 1;
+const addElement = function () {
+    const div = document.createElement('div');
+    div.textContent = number;
+    document.body.appendChild(div);
+    number++;
+}
+btn.addEventListener("click", addElement);
